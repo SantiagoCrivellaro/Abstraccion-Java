66450861f73f420f5816eb458399618cbbd935c5
Santiago Crivellaro <crivellarosantiago1@gmail.com>
2024-11-28 22:49:13 -0300

    First Commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Encapsulamiento;
+
+/**
+ *
+ * @author Santi
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        Programador programador = new Programador();
+        
+        programador.Comer();
+    }
+}
---
+++ Programador.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Encapsulamiento;
+
+
+public class Programador extends Persona {
+    
+    public void Comer(){
+        System.out.println("Un programadro Come tambien eh.");
+    }
+}
